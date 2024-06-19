b35d46aed560cb54ad4fd30a018ec3c82cab8190
Nuno Santos <n.ngan@outlook.com>
2024-06-19 23:40:16 +0100

    Lesson 47 & 48 - Storing Component & Respective Style

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,35 +1,6 @@
-import image from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data";
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function generateRandom(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const dynamicMessage = reactDescriptions[generateRandom(2)];
-
-  return (
-    <header>
-      <img src={image} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {dynamicMessage} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function Concepts(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import Header from './components/Header/Header'
+import Concepts from './components/Concepts/Concepts'
 
 function App() {
   return (
