40325465e493f5f454a55611e3e873f3e2821094
Nuno Santos <n.ngan@outlook.com>
2024-06-19 23:00:37 +0100

    Lesson 42 - Outputting Dynamic Values

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,18 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function generateRandom(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const dynamicMessage = reactDescriptions[generateRandom(2)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {dynamicMessage} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
@@ -14,7 +22,7 @@ function Header() {
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
