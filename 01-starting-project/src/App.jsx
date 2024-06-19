87a5efe34612dac846fabb3153d842b96639b7e9
Nuno Santos <n.ngan@outlook.com>
2024-06-20 00:46:53 +0100

    Lesson 52 - 57 Done
    ◾ Passing custom arguments to event functions
    
    ◾ UI Update
    
    ◾ State Management
    
    ◾ Data output based on state
    
    ◾ Conditional Content Rendering
    
    ◾ Dynamic Styling

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,29 +1,47 @@
-import ConceptsList from './components/Concepts/ConceptsList';
-import Header from './components/Header/Header'
+import { useState } from "react";
+import ConceptsList from "./components/Concepts/ConceptsList";
+import Header from "./components/Header/Header";
 import TabButton from "./components/TabButton/TabButton";
-
-function handleSelect(){
-  console.log("Hello World!")
-}
+import { EXAMPLES } from "./data";
 
 function App() {
+  const [topic, setTopic] = useState(null);
+
+  function handleSelect(selectedTopic) {
+    setTopic(selectedTopic);
+  }
+
   return (
     <div>
       <Header />
       <main>
         <section id="core-concepts">
           <h2>Time to get started!</h2>
-          <ConceptsList/>
+          <ConceptsList />
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>COMPONENTS</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton isSelected={topic === 'components'} onSelect={() => handleSelect("components")}>
+              COMPONENTS
+            </TabButton>
+            <TabButton isSelected={topic === 'jsx'} onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton isSelected={topic === 'props'} onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton isSelected={topic === 'state'} onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          Dynamic Content
+          <div id="tab-content">
+            {!topic ? (
+              <span>Please select a topic.</span>
+            ) : (
+              <>
+                <h3>{EXAMPLES[topic].title}</h3>
+                <p>{EXAMPLES[topic].description}</p>
+                <pre>
+                  <code></code>
+                </pre>
+              </>
+            )}
+          </div>
         </section>
       </main>
     </div>
